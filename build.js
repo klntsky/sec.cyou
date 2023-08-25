b6740f3ed4f26e49d006c8eea299bb898e00bf42
Vladimir Kalnitsky <klntsky@gmail.com>
2023-08-26 02:12:32 +0400

    Better UI

--- build.js
+++ build.js
@@ -4,7 +4,7 @@ import ColorHash from 'color-hash';
 
 const list = JSON.parse(fs.readFileSync('./list.json'));
 const template = fs.readFileSync('./template.ejs').toString();
-const colorHash = new ColorHash.default({ lightness: [0.6, 0.9] });
+const colorHash = new ColorHash.default({ lightness: [0.8, 0.9] });
 
 fs.writeFileSync(
     'index.html',
