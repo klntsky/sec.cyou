621ff616ad9914405c227e3e8654a2325c871011
Vladimir Kalnitsky <klntsky@gmail.com>
2023-08-26 14:13:37 +0400

    Expand data, add chain logos

--- build.js
+++ build.js
@@ -6,6 +6,14 @@ const list = JSON.parse(fs.readFileSync('./list.json'));
 const template = fs.readFileSync('./template.ejs').toString();
 const colorHash = new ColorHash.default({ lightness: [0.8, 0.9] });
 
+for (let entry of list) {
+    for (let chain of entry.chains) {
+        if (!fs.existsSync('./logos/' + chain + '.svg'))  {
+            throw 'Logo does not exist for ' + chain;
+        }
+    }
+}
+
 fs.writeFileSync(
     'index.html',
     ejs.render(
