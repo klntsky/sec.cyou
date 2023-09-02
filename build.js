94d060b4c60b76e48cbfa324ae4075f3959492da
Vladimir Kalnitsky <klntsky@gmail.com>
2023-09-02 04:32:52 +0400

    Use json-schema to validate

--- build.js
+++ build.js
@@ -1,6 +1,12 @@
 import * as fs from 'fs';
 import ejs from 'ejs';
 import ColorHash from 'color-hash';
+import Ajv from "ajv";
+import addFormats from "ajv-formats";
+import betterAjvErrors from 'better-ajv-errors';
+
+const ajv = new Ajv({allErrors: true});
+addFormats(ajv);
 
 const shuffle = raw =>
       raw.map(value => ({ value, sort: Math.random() }))
@@ -8,6 +14,23 @@ const shuffle = raw =>
       .map(({ value }) => value);
 
 const list = shuffle(JSON.parse(fs.readFileSync('./list.json')));
+
+// validate
+const schema = JSON.parse(
+    fs.readFileSync('./schema.json')
+);
+const validate = ajv.compile(schema);
+
+const valid = ajv.validate(schema, list);
+
+if (!valid) {
+    const output = betterAjvErrors(schema, list, validate.errors, { format: 'cli', indent: 2});
+    console.log(output);
+    process.exit(1);
+} else {
+    console.log("valid according to the schema");
+}
+
 const template = fs.readFileSync('./template.ejs').toString();
 const colorHash = new ColorHash.default({ lightness: [0.8, 0.9] });
 
