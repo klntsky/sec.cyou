8682d01374f3c06b0d430768d4404b1eebcf05d5
Vishtar <viktor.kishchuk@gmail.com>
2023-09-04 23:28:33 +0400

    tag tooltips

---
+++ src/components/Tag/constants/tooltipTextMock.js
@@ -0,0 +1,1 @@
+export const tooltipTextMock = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Urna nunc id cursus metus aliquam eleifend mi in."
--- src/components/Tag/index.jsx
+++ src/components/Tag/index.jsx
@@ -1,24 +1,32 @@
+import Tippy from '@tippyjs/react';
+import 'tippy.js/dist/tippy.css';
+
 import { useTagColors } from '../../contexts/tagColors';
 import { classNameByObject } from '../../helpers/classNameByObject';
+import { tooltipsByTag } from './constants/tooltipsByTag'
+import { tooltipTextMock } from './constants/tooltipTextMock'
 
 import './style.css';
 
 export const Tag = ({ isActive, isFiltered, onClick, children }) => {
     const tagColors = useTagColors();
+    const tooltipText = tooltipsByTag[children] || tooltipTextMock;
 
     return (
-        <span
-            className={classNameByObject({
-                tag: true,
-                isActive,
-                isBlackedOut: !isActive && isFiltered,
-            })}
-            style={{
-                backgroundColor: tagColors[children.toLowerCase()]
-            }}
-            onClick={onClick}
-        >
-            {children}
-        </span>
+        <Tippy content={tooltipText} delay={100} className="tag-tooltip">
+            <span
+                className={classNameByObject({
+                    tag: true,
+                    isActive,
+                    isBlackedOut: !isActive && isFiltered,
+                })}
+                style={{
+                    backgroundColor: tagColors[children.toLowerCase()]
+                }}
+                onClick={onClick}
+            >
+                {children}
+            </span>
+        </Tippy>
     )
 }
