c0ccafbd831d99c4b8bc936ea3924a74aae4a43c
Vishtar <viktor.kishchuk@gmail.com>
2023-09-04 11:48:43 +0400

    Vite.config change base url

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   root: 'src',
-  base: 'https://vishtar.github.io/sec.cyou/',
+  base: 'https://sec.cyou/',
   build: {
     outDir: '../dist',
   },
