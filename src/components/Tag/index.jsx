762c89c1b3b9a0a6f6bccf5bfaab64738987fbf5
Vishtar <viktor.kishchuk@gmail.com>
2023-09-05 17:53:55 +0400

    Remove tooltipTextMock

--- src/components/Tag/index.jsx
+++ src/components/Tag/index.jsx
@@ -4,14 +4,13 @@ import 'tippy.js/dist/tippy.css';
 import { useTagColors } from '../../contexts/tagColors';
 import { classNameByObject } from '../../helpers/classNameByObject';
 import { tooltipsByTag } from './constants/tooltipsByTag';
-import { tooltipTextMock } from './constants/tooltipTextMock';
 import { isAppRendered } from '../../helpers/isAppRendered';
 
 import './style.css';
 
 export const Tag = ({ isActive, isFiltered, onClick, children }) => {
     const tagColors = useTagColors();
-    const tooltipText = tooltipsByTag[children] || tooltipTextMock;
+    const tooltipText = tooltipsByTag[children];
 
     const tagBlock = (
         <span
@@ -30,7 +29,7 @@ export const Tag = ({ isActive, isFiltered, onClick, children }) => {
     )
 
     // Build time problem: https://github.com/atomiks/tippyjs-react/issues/381
-    if (!isAppRendered())
+    if (!isAppRendered() || !tooltipText)
         return tagBlock;
 
     return (
