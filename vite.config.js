9249a540141e06daa6b0b951f8109dd078898369
Vishtar <viktor.kishchuk@gmail.com>
2023-09-07 14:48:27 +0400

    chainTag user-select: none

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   root: 'src',
   build: {
-    outDir: '../dist',
+    outDir: '../dist/static',
   },
   plugins: [
     react({
