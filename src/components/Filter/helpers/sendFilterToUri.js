360661fcb5a43545a621feeed137b64a4667304c
Vishtar <viktor.kishchuk@gmail.com>
2023-09-07 14:21:41 +0400

    Tootips interactivity, delete hash while filters clear

--- src/components/Filter/helpers/sendFilterToUri.js
+++ src/components/Filter/helpers/sendFilterToUri.js
@@ -1,13 +1,15 @@
-import { serializationStringForURI } from '.'
+import { serializationStringForURI, removeUrlHash } from '.'
 
 export const sendFilterToUri = ({ filteredTags, filteredChains }) => {
     let newFilterUrl = '';
     if (filteredTags.length || filteredChains.length) {
         const newHashTags = filteredTags.map(filteredTag => serializationStringForURI(filteredTag))
         const newHashChains = filteredChains.map(filteredChain => serializationStringForURI(filteredChain))
-        newFilterUrl = [newHashTags.join(','), newHashChains.join(',')]
-            .filter(Boolean) // To avoid usefull comma after an empty array
-            .join(',');
+        newFilterUrl = newHashTags.concat(newHashChains).join(',');
+    }
+    if (!newFilterUrl) {
+        removeUrlHash();
+        return;
     }
     location.hash = newFilterUrl;
 }
--- src/components/Tag/index.jsx
+++ src/components/Tag/index.jsx
@@ -1,10 +1,9 @@
-import Tippy from '@tippyjs/react';
-import 'tippy.js/dist/tippy.css';
-import classnames from 'classnames'
+import classnames from 'classnames';
+
+import { Tooltip, TagBlock } from '..';
 
 import { useTagColors } from '../../contexts/tagColors';
 import { tooltipsByTag } from './constants/tooltipsByTag';
-import { isAppRendered } from '../../helpers/isAppRendered';
 
 import './style.css';
 
@@ -12,25 +11,21 @@ export const Tag = ({ isActive, isFiltered, onClick, children }) => {
     const tagColors = useTagColors();
     const tooltipText = tooltipsByTag[children];
 
-    const tagBlock = (
-        <span
-            className={classnames('tag', { isBlackedOut: !isActive && isFiltered })}
-            style={{
-                backgroundColor: tagColors[children.toLowerCase()]
-            }}
-            onClick={onClick}
-        >
-            {children}
-        </span>
-    )
-
-    // Build time problem: https://github.com/atomiks/tippyjs-react/issues/381
-    if (!isAppRendered() || !tooltipText)
-        return tagBlock;
-
     return (
-        <Tippy content={tooltipText} delay={100} className="tag-tooltip">
-            {tagBlock}
-        </Tippy>
+        <Tooltip
+            content={tooltipText}
+            delay={300}
+            className="tag-tooltip"
+            interactive={true}
+            interactiveDebounce={500}
+        >
+            <TagBlock
+                className={classnames({ isBlackedOut: !isActive && isFiltered })}
+                backgroundColor={tagColors[children.toLowerCase()]}
+                onClick={onClick}
+            >
+                {children}
+            </TagBlock>
+        </Tooltip>
     )
 }
