88335248d1d49c90398e7455cd41c3472b177737
Vishtar <viktor.kishchuk@gmail.com>
2023-09-07 17:26:12 +0400

    vite.config: dist/static -> public

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   root: 'src',
   build: {
-    outDir: '../dist/static',
+    outDir: '../public',
   },
   plugins: [
     react({
